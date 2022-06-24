e2199ba0cc0cb01160d26770c54e95cd8fc7e312
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-24 03:28:41 +0000

    ok toke 🦜

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 // This file contains the JS functions for index.html
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Unit6-04-HTML/sw.js", {
